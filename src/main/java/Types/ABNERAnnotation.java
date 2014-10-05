5c9d00b0ca1c024d2d40db380bed704cd589539b
amaliujia <amaliujia@163.com>
2014-10-04 23:03:49 -0400

    11

---
+++ src/main/java/Types/ABNERAnnotation.java
@@ -0,0 +1,123 @@
+
+
+/* First created by JCasGen Sat Oct 04 23:03:21 EDT 2014 */
+package Types;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Sat Oct 04 23:03:21 EDT 2014
+ * XML source: /Users/amaliujia/Documents/workspace/hw2-ruiw1/src/main/resources/TypeSystems/SDTypeSystem.xml
+ * @generated */
+public class ABNERAnnotation extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(ABNERAnnotation.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected ABNERAnnotation() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public ABNERAnnotation(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public ABNERAnnotation(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public ABNERAnnotation(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: sentenceID
+
+  /** getter for sentenceID - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getSentenceID() {
+    if (ABNERAnnotation_Type.featOkTst && ((ABNERAnnotation_Type)jcasType).casFeat_sentenceID == null)
+      jcasType.jcas.throwFeatMissing("sentenceID", "Types.ABNERAnnotation");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((ABNERAnnotation_Type)jcasType).casFeatCode_sentenceID);}
+    
+  /** setter for sentenceID - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setSentenceID(String v) {
+    if (ABNERAnnotation_Type.featOkTst && ((ABNERAnnotation_Type)jcasType).casFeat_sentenceID == null)
+      jcasType.jcas.throwFeatMissing("sentenceID", "Types.ABNERAnnotation");
+    jcasType.ll_cas.ll_setStringValue(addr, ((ABNERAnnotation_Type)jcasType).casFeatCode_sentenceID, v);}    
+   
+    
+  //*--------------*
+  //* Feature: entity
+
+  /** getter for entity - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getEntity() {
+    if (ABNERAnnotation_Type.featOkTst && ((ABNERAnnotation_Type)jcasType).casFeat_entity == null)
+      jcasType.jcas.throwFeatMissing("entity", "Types.ABNERAnnotation");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((ABNERAnnotation_Type)jcasType).casFeatCode_entity);}
+    
+  /** setter for entity - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setEntity(String v) {
+    if (ABNERAnnotation_Type.featOkTst && ((ABNERAnnotation_Type)jcasType).casFeat_entity == null)
+      jcasType.jcas.throwFeatMissing("entity", "Types.ABNERAnnotation");
+    jcasType.ll_cas.ll_setStringValue(addr, ((ABNERAnnotation_Type)jcasType).casFeatCode_entity, v);}    
+  }
+
+    
---
+++ src/main/java/Types/ABNERAnnotation_Type.java
@@ -0,0 +1,121 @@
+
+/* First created by JCasGen Sat Oct 04 23:03:21 EDT 2014 */
+package Types;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Sat Oct 04 23:03:21 EDT 2014
+ * @generated */
+public class ABNERAnnotation_Type extends Annotation_Type {
+  /** @generated 
+   * @return the generator for this type
+   */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /** @generated */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (ABNERAnnotation_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = ABNERAnnotation_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new ABNERAnnotation(addr, ABNERAnnotation_Type.this);
+  			   ABNERAnnotation_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new ABNERAnnotation(addr, ABNERAnnotation_Type.this);
+  	  }
+    };
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = ABNERAnnotation.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("Types.ABNERAnnotation");
+ 
+  /** @generated */
+  final Feature casFeat_sentenceID;
+  /** @generated */
+  final int     casFeatCode_sentenceID;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getSentenceID(int addr) {
+        if (featOkTst && casFeat_sentenceID == null)
+      jcas.throwFeatMissing("sentenceID", "Types.ABNERAnnotation");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_sentenceID);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setSentenceID(int addr, String v) {
+        if (featOkTst && casFeat_sentenceID == null)
+      jcas.throwFeatMissing("sentenceID", "Types.ABNERAnnotation");
+    ll_cas.ll_setStringValue(addr, casFeatCode_sentenceID, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_entity;
+  /** @generated */
+  final int     casFeatCode_entity;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getEntity(int addr) {
+        if (featOkTst && casFeat_entity == null)
+      jcas.throwFeatMissing("entity", "Types.ABNERAnnotation");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_entity);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setEntity(int addr, String v) {
+        if (featOkTst && casFeat_entity == null)
+      jcas.throwFeatMissing("entity", "Types.ABNERAnnotation");
+    ll_cas.ll_setStringValue(addr, casFeatCode_entity, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public ABNERAnnotation_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_sentenceID = jcas.getRequiredFeatureDE(casType, "sentenceID", "uima.cas.String", featOkTst);
+    casFeatCode_sentenceID  = (null == casFeat_sentenceID) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_sentenceID).getCode();
+
+ 
+    casFeat_entity = jcas.getRequiredFeatureDE(casType, "entity", "uima.cas.String", featOkTst);
+    casFeatCode_entity  = (null == casFeat_entity) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_entity).getCode();
+
+  }
+}
+
+
+
+    
